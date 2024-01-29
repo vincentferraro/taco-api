42d90e86c0660a1397f734a19458146756048774
vincentferraro <v.ferraro@live.fr>
2024-01-29 10:58:17 -0500

    WIP:adding security and improving Ingredient Controller

--- src/main/java/taco/api/com/api/controllers/IngredientController.java
+++ src/main/java/taco/api/com/api/controllers/IngredientController.java
@@ -8,6 +8,7 @@ import taco.api.com.api.repositories.IngredientRepository;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,29 +17,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Optional;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
-// @RestController
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
 @RequestMapping(path="ingredients", produces={ "application/json","text/xml"})
 @CrossOrigin(origins="http://tacocloud:8080")
 
 public class IngredientController {
+
     private IngredientRepository ingredientRepo;
 
-    RestTemplate res = new RestTemplate();
-
-    
     IngredientController(IngredientRepository indregientRepo){
         this.ingredientRepo = indregientRepo;
     }
 
     @GetMapping
-    public String getWelcomeReponse() {
-        return "Hello ingredient routes";
-    }
-
-    @GetMapping("/all")
-    public Iterable<Ingredient> getIngredientAll() {
+    public Iterable<Ingredient> getAllIngredients() {
         return ingredientRepo.findAll();
     }
 
@@ -52,5 +51,14 @@ public class IngredientController {
         return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
     
+    @PostMapping
+    public Ingredient saveIngredient(@RequestBody Ingredient ingredient) {
+        return ingredientRepo.save(ingredient);
+    }
+
+    @DeleteMapping(path="/{id}")
+    public void deleteIngredient(@PathVariable("id") String id){
+        ingredientRepo.deleteById(id);
+    }
     
 }
---
+++ src/main/java/taco/api/com/api/security/SecurityConfig.java
@@ -0,0 +1,16 @@
+package taco.api.com.api.security;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+public class SecurityConfig {
+    
+
+
+// @Override
+// protected void configure(HttpSecurity http) throws Exception {
+//   http
+//     .authorizeRequests()
+// .antMatchers(HttpMethod.POST, "/ingredients").hasRole("ADMIN") .antMatchers(HttpMethod.DELETE, "/ingredients/**").hasRole("ADMIN")
+// ... }
+}
